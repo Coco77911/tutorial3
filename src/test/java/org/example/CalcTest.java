c411542121248dc134cdc040a3f7f3d7531a0abc
Emma Paris <eparis@atlassian.com>
2026-09-22 15:07:37 +0800

    add failing testSubtraction test case

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -7,6 +7,11 @@ public class CalcTest {
   Calc c = new Calc();
 
     @Test
+    public void testSubtraction(){
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4,2));
+    }
+
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
